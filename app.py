971920800cfe1ca8ff4c2da674e35650186bddeb
Shubham-nebula <129153839+Shubham-nebula@users.noreply.github.com>
2023-07-22 21:01:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ def download_file():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the app on 0.0.0.0:8000
+    app.run(host="0.0.0.0", port=8000, debug=True)
