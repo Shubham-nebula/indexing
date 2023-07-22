bf35b37c83cea0b3aa102a195ace770532a86e1c
Shubham-nebula <129153839+Shubham-nebula@users.noreply.github.com>
2023-07-22 20:52:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import SentenceTransformerEmbeddings
 import pinecone
 from langchain.vectorstores import Pinecone
+from azure.storage.blob import BlobServiceClient, BlobClient
+import os
 
 app = Flask(__name__)
 
@@ -33,7 +35,7 @@ def process_documents(pdf_directory):
 
     return "Documents processed and indexed successfully."
 
-# Endpoint to handle incoming requests
+# Endpoint to handle incoming requests for processing documents
 @app.route('/process_documents', methods=['POST'])
 def process_documents_route():
     try:
@@ -50,6 +52,46 @@ def process_documents_route():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+# Function to download file from Azure Blob Storage
+def download_file_from_blob(container_name, blob_name):
+    try:
+        connection_string = "DefaultEndpointsProtocol=https;AccountName=azuretestshubham832458;AccountKey=2yEaP59qlgKVv6kEUCA5ARB4wdV3ZRoL2X9zjYCcIxOSYAG1CSBbBlAMPx3uBIe7ilQtSh7purEK+AStvFn8GA==;EndpointSuffix=core.windows.net"  # Replace with your Azure Blob Storage connection string
+        
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+        
+        destination_path = f"transcripts/{blob_name}"  # Replace with the desired destination path
+        
+        os.makedirs(os.path.dirname(destination_path), exist_ok=True)  # Create the destination directory if it doesn't exist
+        
+        with open(destination_path, "wb") as file:
+            file.write(blob_client.download_blob().readall())
+        
+        print(f"File downloaded successfully: {destination_path}")
+        return True
+    except Exception as e:
+        print(f"Error downloading file '{blob_name}': {str(e)}")
+        return False
+
+# Endpoint to handle incoming requests for downloading files from Azure Blob Storage
+@app.route("/download", methods=["POST"])
+def download_file():
+    try:
+        payload = request.json
+        blob_name = payload.get("blob_name")
+        
+        if not blob_name:
+            return jsonify({"message": "Blob name not provided."}), 400
+        
+        success = download_file_from_blob("transcript", blob_name)
+        
+        if success:
+            return jsonify({"message": "File download completed successfully."})
+        else:
+            return jsonify({"message": "File download failed."}), 500
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
